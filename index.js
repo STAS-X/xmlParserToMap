e64347b2c77946c9773a3903383414d895c9e656
STAS-X <rosteh82@gmail.com>
2024-03-11 17:09:11 +0300

    add loader

--- index.js
+++ index.js
@@ -10,16 +10,28 @@ window.onload = () => {
 };
 
 // находим кнопку и добавляем к ней обработчик
-document.getElementById('add_poly').onclick = async () => {
+document.getElementById('add_poly').onclick = async function () {
 	//buildContextFromXml('forestReforestationSample.xml');
+	this.disabled = true;
+	const loader = document.querySelector('#loader');
+	loader.textContent = '';
+	loader.classList.toggle('loader');
+
 	const polygonData = await buildContextFromXml('forestReforestationSample.xml');
 	addPolygonToMap(polygonData);
+
+	// Через 1 сек запускаем функцию переключения лоадера на текст
+	setTimeout(() => {
+		loader.classList.toggle('loader');
+		this.disabled = false;
+		loader.textContent = 'Добавить полигон';
+	}, 1000);
 };
 
 const success = ({ coords }) => {
 	const { latitude, longitude } = coords;
 	const currentPosition = [latitude, longitude];
-	console.log(currentPosition, 'get current position');
+	//console.log(currentPosition, 'get current position');
 	// после загрузки документа открываем новую карту, передавая ей текущую позицию
 	openNewMap(currentPosition);
 };
